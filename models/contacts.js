b284c11c4e1b3eca35530cf6be3c0505b1360422
Andrew-aka-Lemon <105217029+Andrew-aka-Lemon@users.noreply.github.com>
2023-02-18 23:40:52 +0200

    hw 1 methods added

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,58 @@
 // const fs = require('fs/promises')
 
-const listContacts = async () => {}
+const listContacts = async () => {
+  let contacts = [];
+  try {
+    contacts = JSON.parse(await fs.readFile(contactsPath, 'utf8'));
+  } catch (error) {
+    console.log("Can't read Contacts!");
+  }
 
-const getContactById = async (contactId) => {}
+  return contacts;
+};
 
-const removeContact = async (contactId) => {}
+const getContactById = async contactId => {
+  const contacts = await listContacts();
+  const choosenContact = contacts.filter(contact => contact.id === contactId);
+  if (choosenContact.length === 0) {
+    console.log('There is no book with such ID !');
+    return null;
+  }
+  return choosenContact;
+};
 
-const addContact = async (body) => {}
+const removeContact = async contactId => {
+  const contacts = await listContacts();
 
-const updateContact = async (contactId, body) => {}
+  const withoutChoosenContact = contacts.filter(
+    contact => contact.id !== contactId
+  );
+
+  if (contacts.length === withoutChoosenContact.length) {
+    console.log('There is no contact with such ID !');
+    return null;
+  }
+
+  fs.writeFile(contactsPath, JSON.stringify(withoutChoosenContact, null, 2));
+
+  return withoutChoosenContact;
+};
+
+const addContact = async body => {
+  const id = nanoid();
+  const { name, email, phone } = body;
+  const newContact = { id, name, email, phone };
+
+  const contacts = await listContacts();
+
+  const newContacts = [...contacts, newContact];
+
+  fs.writeFile(contactsPath, JSON.stringify(newContacts, null, 2));
+
+  return newContacts;
+};
+
+const updateContact = async (contactId, body) => {};
 
 module.exports = {
   listContacts,
@@ -16,4 +60,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};

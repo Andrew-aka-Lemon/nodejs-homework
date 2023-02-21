776539707a990dc0549eb54686c966259c285052
Andrew-aka-Lemon <105217029+Andrew-aka-Lemon@users.noreply.github.com>
2023-02-22 00:37:52 +0200

    trycathes removed

--- controllers/books.js
+++ controllers/books.js
@@ -15,72 +15,56 @@ async function getAll(req, res) {
 }
 
 async function getById(req, res, next) {
-  try {
-    const contactById = await contacts.getContactById(req.params.contactId);
+  const contactById = await contacts.getContactById(req.params.contactId);
 
-    if (contactById === null) {
-      throw HttpError(404, 'Not found');
-    }
-    res.status(200).json(contactById);
-  } catch (error) {
-    next(error);
+  if (contactById === null) {
+    throw HttpError(404, 'Not found');
   }
+  res.status(200).json(contactById);
 }
 
 async function addContact(req, res, next) {
-  try {
-    const { error } = schema.validate(req.body);
-    console.log(error);
-    if (error) {
-      throw HttpError(400, 'missing required name field');
-    }
-
-    const newContact = await contacts.addContact(req.body);
-
-    res.status(201).json(newContact);
-  } catch (error) {
-    next(error);
+  const { error } = schema.validate(req.body);
+  console.log(error);
+  if (error) {
+    throw HttpError(400, 'missing required name field');
   }
+
+  const newContact = await contacts.addContact(req.body);
+
+  res.status(201).json(newContact);
 }
 
 async function removeById(req, res, next) {
-  try {
-    const contactById = await contacts.removeContact(req.params.contactId);
+  const contactById = await contacts.removeContact(req.params.contactId);
 
-    if (contactById === null) {
-      throw HttpError(404, 'Not found');
-    }
-    res.status(200).json(contactById);
-  } catch (error) {
-    next(error);
+  if (contactById === null) {
+    throw HttpError(404, 'Not found');
   }
+  res.status(200).json(contactById);
 }
 
 async function updateById(req, res, next) {
-  try {
-    const contactById = await contacts.getContactById(req.params.contactId);
+  const contactById = await contacts.getContactById(req.params.contactId);
 
-    if (contactById === null) {
-      throw HttpError(404, 'Not found');
-    }
-
-    const { error } = schema.validate(req.body);
-
-    if (error) {
-      throw HttpError(400, 'missing required name field');
-    }
-
-    const updatedContact = await contacts.updateContact(
-      req.params.contactId,
-      req.body
-    );
-
-    console.log(updatedContact);
-
-    res.status(200).json(updatedContact);
-  } catch (error) {
-    next(error);
+  if (contactById === null) {
+    throw HttpError(404, 'Not found');
   }
+
+  const { error } = schema.validate(req.body);
+
+  if (error) {
+    throw HttpError(400, 'missing required name field');
+  }
+
+  const updatedContact = await contacts.updateContact(
+    req.params.contactId,
+    req.body
+  );
+
+  console.log(updatedContact);
+
+  res.status(200).json(updatedContact);
 }
 
 module.exports = {
